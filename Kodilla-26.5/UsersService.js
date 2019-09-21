c89619233698d42be257bf91edea5ef26d56af9d
Bartek Bugała <bartek.bugala@gmail.com>
2019-09-21 20:14:54 +0200

    task 26.5

---
+++ Kodilla-26.5/UsersService.js
@@ -0,0 +1,29 @@
+class UsersService {
+  constructor() {
+    this.users = [];
+    this.userCounter = 0;
+  }
+
+  getAllUsers() {
+    return this.users;
+  }
+
+  getUserById(userId) {
+    return this.users.find(user => user.id === userId);
+  }
+
+  addUser(user) {
+    this.userCounter++;
+
+    let usersArray = this.users.map(el => el.name);
+    if (usersArray.includes(user.name)) {
+      user.name = `${user.name} - ${this.userCounter}`;
+    }
+    this.users = [user, ...this.users];
+  }
+
+  removeUser(userId) {
+    this.users = this.users.filter(user => user.id !== userId);
+  }
+}
+module.exports = UsersService;
---
+++ Kodilla-26.5/webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const OptimizeJsPlugin = require('optimize-js-plugin');
+
+const plugins = [
+  new HtmlWebpackPlugin({
+    template: 'client/index.html',
+    filename: 'index.html',
+    inject: 'body'
+  })
+];
+
+module.exports = env => {
+  const environment = env || 'production';
+  if (env === 'production') {
+    plugins.push(
+      new OptimizeJsPlugin({
+        sourceMap: false
+      })
+    );
+  }
+
+  return {
+    mode: environment,
+    entry: './client/index.js',
+    output: {
+      path: path.resolve(__dirname, 'public'),
+      filename: 'app.bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/,
+          options: {
+            plugins: env !== 'production' ? ['react-hot-loader/babel'] : []
+          }
+        },
+        {
+          test: /\.css$/,
+          use: [
+            {
+              loader: 'style-loader'
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+                sourceMap: true,
+                importLoaders: 1
+              }
+            }
+          ]
+        }
+      ]
+    },
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      compress: true,
+      port: 8080,
+      proxy: {
+        '/socket.io': {
+          target: 'http://localhost:3000',
+          ws: true
+        }
+      }
+    },
+    plugins: plugins
+  };
+};
